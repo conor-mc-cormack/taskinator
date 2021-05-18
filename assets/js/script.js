6e0f94a35503193869e353ed0a5abf8ddef3366f
Conor-Mc-Cormack <c.mccormack3000@gmail.com>
2021-05-18 16:39:47 -0700

    fixed submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // its very important to place this function above the
 // event listener because we would recive a error that
 // createTaskHandler hasnt been defined because were calling
 // a function not defined yet
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+  event.preventDefault();
+
   // create new task item
   var listItemEl = document.createElement("li");
   // add item to stylesheet
@@ -14,7 +16,8 @@ var createTaskHandler = function () {
   listItemEl.textContent = "This is a new task.";
   // add the element as a attachment to the task
   tasksToDoEl.appendChild(listItemEl);
+  console.log(event);
 };
 
 // this defines the the function
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("click", createTaskHandler);
